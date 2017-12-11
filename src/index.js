751b9f00130ba898778aaec0d16976574e0ebb89
Abdul Moiz Lakhani <lakhaniabdulmoiz@gmail.com>
2017-12-11 06:15:13 -0800

    Minor Changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import $ from 'jquery';
+import PopperJS from 'popper.js';
 import AppRoute from './Routes';
 import registerServiceWorker from './registerServiceWorker';
 
