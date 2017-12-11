e99354bdfc9195c0126e4254fdb0939bb6c343dc
Abdul Moiz Lakhani <lakhaniabdulmoiz@gmail.com>
2017-12-11 05:55:35 -0800

    Logo Embedded

--- src/containers/mainPage.js
+++ src/containers/mainPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 // import PollingImage from './../images/polling.png';
+import PollingSymbol from './../images/pollsymbol.png';
 
 import './MainPage.css'
 
@@ -8,6 +9,10 @@ class MainPage extends Component {
         return (
             <div className="mainPageBgImage">
                 <div className="mainContainer">
+                    <div className="pollingLogoContainer">
+                        <img src={PollingSymbol} />
+                        <h1>Polling App</h1>
+                    </div>
                 </div>
             </div>
         )
